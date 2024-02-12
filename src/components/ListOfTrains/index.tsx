dd71ca0d779553d498b5a4a48ee58a82a27d7d73
Daria <zhezherya_d@mail.ru>
2024-02-13 01:09:13 +0300

    edit styles again

--- src/components/ListOfTrains/index.tsx
+++ src/components/ListOfTrains/index.tsx
@@ -5,6 +5,7 @@ import './index.css'
 const ListOfTrains = (props: propsTrain ) => {
 
     return <ul className="trains">
+        <p>Все поезда</p>
         {props.trains.map((train, id) => {
             return <li className="trains-link" key={id} >
                 <Link  to={`/trainCharacteristic/${id}`}>{train.name} {train.description}</Link>
